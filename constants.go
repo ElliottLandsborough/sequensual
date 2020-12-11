a10dc03361d8f779bb9ecd5f45a4956562dc1a8a
Chewy Jetpack <chewyjetpack@gmail.com>
2020-12-11 14:48:38 +0000

    OSC test - plays a single note when run after starting sushi

--- constants.go
+++ constants.go
@@ -1,22 +1,22 @@
 package main
 
-// Pulses Per Quarter Note.  A synchronization primative used in music technology.
+// Ppqn - Pulses Per Quarter Note.  A synchronization primative used in music technology.
 var Ppqn = 24.0
 
-// The number of seconds in a Minute as a float.
+// Minute - The number of seconds in a Minute as a float.
 var Minute = 60.0
 
-// The number of microseconds in a Second.
+// Microsecond - The number of microseconds in a Second.
 var Microsecond = 1000000000
 
-// The Default BPM of the step sequencer
+// DefaultTempo - The Default BPM of the step sequencer
 var DefaultTempo = 120.0
 
-// The standard audio sampling rate of 44.1kHz (44100)
-var SampleRate = 44100
+// SampleRate - The standard audio sampling rate of 48kHz (48000)
+var SampleRate = 48000
 
-// The number of portaudio input channels.  This value is set to 0 since we're reading from disk.
+// InputChannels - The number of portaudio input channels.  This value is set to 0 since we're reading from disk.
 var InputChannels = 0
 
-// The number of portaudio output channels.
+// OutputChannels - The number of portaudio output channels.
 var OutputChannels = 2
